a03edbb92cdcd9b2b194599647209cd13048ab9d
Torben Buck <dev@tbuck.de>
2026-09-20 22:51:40 +0200

    Release v1.0.1

--- app.js
+++ app.js
@@ -207,7 +207,7 @@ const STRINGS = {
     tagsApplied: (n) => `Album und Interpret bei ${n} Titeln gesetzt`,
   },
 };
-export const APP_VERSION = '1.0.0';
+export const APP_VERSION = '1.0.1';
 
 /* ---------- settings ---------- */
 const SETTINGS_KEY = 'audioTag.settings';
